68e833b44c8cbbc801ae80a6e793719b17458720
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-29 20:07:19 -0500

    pushing abduls changes

--- gai-app/src/App.js
+++ gai-app/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <BrowserRouter>
+              <Navbar />
+
+
+              <Switch>
+                <Route path = '/' exact component = {LandingPage} />
+                {/* <Route path ='/more-info' component={} />  */}
+              </Switch>
+          </BrowserRouter> 
       </header>
     </div>
   );
