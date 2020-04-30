0e6fe7d40bc47f337a1dfc463f345bbfb8453b93
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-30 18:37:16 -0400

    home.scss

--- gai-app/src/components/Author.js
+++ gai-app/src/components/Author.js
@@ -14,28 +14,27 @@ export default class  Author extends React.Component{
     render(){
       return (
         <div id="author-div">
-          <h1 id='author-name-h1' onClick={this. bioPopUp}>
+          <h1 id='author-name-h1'>
           Mark Latonero
           </h1>
+            <div id="bio-and-photo-div">
             <p id="bio-p">
             "Mark Latonero is a Senior Researcher at Data & Society focused on human rights. He is a fellow at Harvard Kennedy School’s Carr Center for Human Rights Policy and a research fellow at both Berkeley Law’s Human Rights Center and USC’s Annenberg Center for Communication Leadership & Policy. Previously he was a research director and research professor at USC where he led the Technology and Human Trafficking Initiative. He has also served as the Innovation Consultant for the UN Office of the High Commissioner for Human Rights."
             <a id="data-and-society-link" href="https://datasociety.net/people/latonero-mark/"> Data & Society</a>
             </p>
             
           <img id="author-photo" src={require('../img/profile.jpg')}  alt='profile pic'/>
-
+          </div>
           <ul id='author-contacts-ul'>
-                <li>
-                    <a href='https://twitter.com/latonero?lang=en'>Twitter</a>
+                <li className="author-contacts-li">
+                    <a className="author-contacts-links" href='https://twitter.com/latonero?lang=en'>Twitter</a>
                 </li>
 
-                <li>
-                    <a href='https://www.linkedin.com/in/marklatonero'>LinkedIn</a>
+                <li className="author-contacts-li">
+                    <a className="author-contacts-links" href='https://www.linkedin.com/in/marklatonero'>LinkedIn</a>
                 </li>
-              
-
-               <li>
-                     <a href='https://latonero.com/'>Personal WebPage</a>
+               <li className="author-contacts-li" >
+                     <a className="author-contacts-links" href='https://latonero.com/'>Personal WebPage</a>
                </li>
 
             </ul>
