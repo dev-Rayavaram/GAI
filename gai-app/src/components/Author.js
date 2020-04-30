95bafb1ba0693a7cd1c88dccbb1665273f55eafc
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-30 18:12:40 -0400

    home.scss

--- gai-app/src/components/Author.js
+++ gai-app/src/components/Author.js
@@ -13,18 +13,18 @@ export default class  Author extends React.Component{
   
     render(){
       return (
-        <div id="author">
-          <h1 className='authorsName' onClick={this. bioPopUp}>
+        <div id="author-div">
+          <h1 id='author-name-h1' onClick={this. bioPopUp}>
           Mark Latonero
           </h1>
-          
-             Mark Latonero is a Senior Researcher at Data & Society focused on human rights. ... Previously he was a research director and research professor at USC where he led the Technology and Human Trafficking Initiative. He has also served as the Innovation Consultant for the UN Office of the High Commissioner for Human Rights." -google
-              
-          
-          
-          {/* <img src={require('../img/profile.jpg')}  alt='profile pic'/> */}
+            <p id="bio-p">
+            "Mark Latonero is a Senior Researcher at Data & Society focused on human rights. He is a fellow at Harvard Kennedy School’s Carr Center for Human Rights Policy and a research fellow at both Berkeley Law’s Human Rights Center and USC’s Annenberg Center for Communication Leadership & Policy. Previously he was a research director and research professor at USC where he led the Technology and Human Trafficking Initiative. He has also served as the Innovation Consultant for the UN Office of the High Commissioner for Human Rights."
+            <a id="data-and-society-link" href="https://datasociety.net/people/latonero-mark/"> Data & Society</a>
+            </p>
+            
+          <img id="author-photo" src={require('../img/profile.jpg')}  alt='profile pic'/>
 
-          <ul className='contacts'>
+          <ul id='author-contacts-ul'>
                 <li>
                     <a href='https://twitter.com/latonero?lang=en'>Twitter</a>
                 </li>
